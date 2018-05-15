6ebbdecb1c916d8f6ba555a8edc39416238abe85
ain2108 <ain2108@columbia.edu>
2018-05-14 20:45:19 -0400

    removed the gloabal winners array adn swapped it out for a local solution.

--- contracts/Game.sol
+++ contracts/Game.sol
@@ -131,16 +131,15 @@ contract Game is Ownable, GameHelper {
     mapping (address => bytes32) public commits;
     mapping (address => uint) public game_data;
     address[] internal player_addrs;
-    address[] internal winners;
+    //address[] internal winners;
 
     // UI vars
-    address[] public last_winners;
+    //address[] public last_winners;
 
     ////// DEBUG vars and debug functions
     uint public average23 = 0;
 
     function set_MAX_PLAYERS(uint new_val) public onlyOwner {
-        //MAX_PLAYERS = new_val;
         config.MAX_PLAYERS = new_val;
     }
 
@@ -193,9 +192,14 @@ contract Game is Ownable, GameHelper {
         string guess,
         string random
     );
+
+    event DebugEvent(
+        string error
+    );
     function reveal(string guess, string random) public  {
         
         //require(game_state == GameState.REVEAL_STATE);
+        emit DebugEvent("Entered reveal");
         require(state.gameState == GameState.REVEAL_STATE);
         
         // DEBUG: Need to make sure it throws if the guess is not integer
@@ -213,7 +217,10 @@ contract Game is Ownable, GameHelper {
 
         emit SuccesfulReveal(guess, random);
 
+        emit DebugEvent("All good here");
+
         if(state.currNumberReveals == config.MAX_PLAYERS){
+            emit SuccesfulReveal(guess, random);
             find_winner();
         }
     }
@@ -223,7 +230,7 @@ contract Game is Ownable, GameHelper {
 
     function find_winner() private {
 
-        emit DebugWinner(5, player_addrs.length);
+        emit DebugWinner(1, player_addrs.length);
         
         // Calculate the 2/3 average
         uint guess_sum = 0;
@@ -239,6 +246,11 @@ contract Game is Ownable, GameHelper {
 
         //DEBUG
         average23 = twothirdsavg;
+
+        address[] memory winners = new address[](config.MAX_PLAYERS);
+        uint winIndex = 0;
+        // We also flush the last list of winner
+        delete info.lastWinners;
 
 
         // Find the guessers who are the closest to the 2/3 average
@@ -259,25 +271,37 @@ contract Game is Ownable, GameHelper {
             // If the difference is less than the smallest difference,
             // we delete all the winners and add the new candidate
             if(cur_diff < min_diff) {
-                delete winners;
-                winners.push(player_addrs[i]);
+                
+                // NOT A BUG!
+                //delete winners; // WTF you might ask? There is no necessity to delete elements.
+                
+                winIndex = 0;
+                winners[winIndex] = player_addrs[i];
+                winIndex++;
 
                 min_diff = cur_diff;
             // Else, if the difference are the same, we add the candidate to the 
             // list of winners
             } else if(cur_diff == min_diff){
-                winners.push(player_addrs[i]);
+                winners[winIndex] = player_addrs[i];
+                winIndex++;
             }
         }
+
+         emit DebugWinner(2, player_addrs.length);
+
+        // winrIndex here has the number of winner in our array
+        require(winIndex > 0);
 
         // Lets pay ourselves some money
         uint gamefee = (address(this).balance/100) * config.GAME_FEE_PERCENT;
         config.FEE_ADDRESS.transfer(gamefee);
 
         // Split the rest equally among winners
-        uint prize = address(this).balance/winners.length;
-        for(i = 0; i < winners.length; i++){
+        uint prize = address(this).balance/winIndex;
+        for(i = 0; i < winIndex; i++){
             winners[i].transfer(prize); 
+            info.lastWinners.push(winners[i]);
             //emit DebugWinner(winners[i], winners.length);
         }
         info.lastPrize = prize;
@@ -297,11 +321,11 @@ contract Game is Ownable, GameHelper {
         state.gameStateDebug = 0;
 
         delete player_addrs;
-        delete info.lastWinners;
+        //delete info.lastWinners;
         
-        info.lastWinners = winners;
+        //info.lastWinners = winners;
 
-        delete winners;
+        //delete winners;
         
         state.currNumberCommits = 0;
         state.currNumberReveals = 0;
