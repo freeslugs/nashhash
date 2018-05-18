c8451d1e6262b915959314c22b3b23de57d7cb48
ain2108 <ain2108@columbia.edu>
2018-05-18 17:11:54 -0400

    minor

--- test/gameMaster.js
+++ test/gameMaster.js
@@ -1,19 +1,19 @@
-var Web3Utils = require('web3-utils');
-import API from '../src/api/Game.js';
+// var Web3Utils = require('web3-utils');
+// import API from '../src/api/Game.js';
 
-var contractAddress = "DUMMY";
-var contractAbi = ".";
-
-
-
-const contract = web3.eth.contract(contractAbi);
-const game = contract.at(contractAddress);
+// var contractAddress = "DUMMY";
+// var contractAbi = ".";
 
 
-var privateKey = ""
+
+// const contract = web3.eth.contract(contractAbi);
+// const game = contract.at(contractAddress);
 
 
-function callPayout(contractAddr){
-    var ownerAccount = web3.eth.accounts.privateKeyToAccount(privateKey);
-    API.payout(game);
-}
+// var privateKey = ""
+
+
+// function callPayout(contractAddr){
+//     var ownerAccount = web3.eth.accounts.privateKeyToAccount(privateKey);
+//     API.payout(game);
+// }
