4d7e200920f9986f5ac40e6c1fe376f89bc80906
ain2108 <ain2108@columbia.edu>
2018-06-05 14:23:36 -0400

    state consistency tests and operator control framework

--- src/gamemaster/gameoperator.go
+++ src/gamemaster/gameoperator.go
@@ -3,9 +3,11 @@ package gm
 import (
 	"errors"
 	"fmt"
+	"time"
 )
 
 const (
+	// StopGame command to stop a game
 	StopGame = 0
 )
 
@@ -41,13 +43,44 @@ func (gop *GameOperator) Play() error {
 		return errors.New("")
 	}
 	go gop.playGame()
+	gop.playing = true
 	return nil
 }
 
+// Game control logic goes into this function
 func (gop *GameOperator) playGame() {
 	fmt.Printf("playing the game on address %s\n", gop.contractAddress)
-	<-gop.controlChannel
-	fmt.Printf("operator quitting the game at address %s\n", gop.contractAddress)
+	for {
+
+		// We use select here for non-blocking read on the socket
+		select {
+		case cmd := <-gop.controlChannel:
+
+			// Different commands can be passed here.
+			// For example, we can ask the handler to reset the state
+			// or log the current state of the game
+			switch cmd {
+			case StopGame:
+				fmt.Printf("operator quitting the game at address %s\n", gop.contractAddress)
+				return
+			default:
+				fmt.Printf("Unknown command")
+			}
+
+		// The default behaviour is to continue operating the game
+		default:
+			// Proceed with the normal game logic
+			gop.operate()
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
+
+// This function is the heart of operator. Does the following:
+// 1) Figure out in which state the game is
+// 2) Force a state transition or payout
+func (gop *GameOperator) operate() {
+	fmt.Printf("wow look at me I am operating hard %s\n", gop.contractAddress)
 }
 
 // Stop stops the operator from operating the game. The game ideally should also
@@ -57,5 +90,6 @@ func (gop *GameOperator) Stop() error {
 		return errors.New("game already stopped")
 	}
 	gop.controlChannel <- StopGame
+	gop.playing = false
 	return nil
 }
--- src/gamemaster/gameoperator_test.go
+++ src/gamemaster/gameoperator_test.go
@@ -2,12 +2,56 @@ package gm
 
 import (
 	"testing"
+	"time"
 )
 
 func TestOperatorInit(t *testing.T) {
 	var gop GameOperator
 	gop.Init("0x1")
-
 	assertEqual(t, gop.ContractAddress(), "0x1", "Wrong contract address")
+	assertEqual(t, gop.Playing(), false, "Bad state: should not be playing")
+}
+
+func TestPlay(t *testing.T) {
+	var gop GameOperator
+	gop.Init("0x1")
+
+	err := gop.Play()
+	if err != nil {
+		panic("bad")
+	}
+
+	assertEqual(t, gop.Playing(), true, "Bad state: should be playing")
+	time.Sleep(2 * time.Second)
+
+	gop.Stop()
+	time.Sleep(2 * time.Second)
+
+	assertEqual(t, gop.Playing(), false, "Bad state: should not be playing")
+}
+
+func TestRepeatedPlay(t *testing.T) {
+	var gop GameOperator
+	gop.Init("0x1")
+
+	// Call play twice
+	err := gop.Play()
+	if err != nil {
+		panic("bad")
+	}
+
+	err = gop.Play()
+	assertNotEqual(t, err, nil, "Should not allow repeated play")
+	assertEqual(t, gop.Playing(), true, "Bad state: should be playing")
+
+	time.Sleep(2 * time.Second)
+
+	err = gop.Stop()
+	assertEqual(t, err, nil, "Should not allow repeated stop")
+	assertEqual(t, gop.Playing(), false, "Bad state: should not be playing")
+
+	err = gop.Stop()
+	assertEqual(t, gop.Playing(), false, "Bad state: should not be playing")
+	assertNotEqual(t, err, nil, "Should not allow repeated stop")
 
 }
