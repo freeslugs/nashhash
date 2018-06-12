9e346098d1d1402e3e35223d13fcac3422700b6e
Gilad <freeslugs@gmail.com>
2018-06-12 16:46:57 +0300

    forgot to add change to migrations file, fixed

--- migrations/3_game.js
+++ migrations/3_game.js
@@ -16,67 +16,66 @@ var gameAddresses = [];
 
 module.exports = function(deployer) {
 	deployer.deploy(GameFactory).then(async () => {
-
 		const gameFactory = await GameFactory.at(GameFactory.address);
 
 		const TwoThirds1 = await gameFactory.createGame(
 			TWO_THIRDS_TYP,
-	    	0x2540099e9ed04aF369d557a40da2D8f9c2ab928D,
-	        GAME_FEE_PERCENT,
-	        BET_1,
-	        MAX_PLAYERS, 
-	        GAME_STAGE_LENGTH,
-	    );
+    	0x2540099e9ed04aF369d557a40da2D8f9c2ab928D,
+      GAME_FEE_PERCENT,
+      BET_1,
+      MAX_PLAYERS, 
+      GAME_STAGE_LENGTH
+    );
 		gameAddresses.push(TwoThirds1.address);
 
 		const TwoThirds01 = await gameFactory.createGame(
 			TWO_THIRDS_TYP,
-	    	0x2540099e9ed04aF369d557a40da2D8f9c2ab928D,
-	        GAME_FEE_PERCENT,
-	        BET_01,
-	        MAX_PLAYERS, 
-	        GAME_STAGE_LENGTH,
-	    );
+			0x2540099e9ed04aF369d557a40da2D8f9c2ab928D,
+			GAME_FEE_PERCENT,
+			BET_01,
+			MAX_PLAYERS, 
+			GAME_STAGE_LENGTH
+    );
 		gameAddresses.push(TwoThirds01.address);
 
 		const TwoThirds001 = await gameFactory.createGame(
 			TWO_THIRDS_TYP,
-	    	0x2540099e9ed04aF369d557a40da2D8f9c2ab928D,
-	        GAME_FEE_PERCENT,
-	        BET_001,
-	        MAX_PLAYERS, 
-	        GAME_STAGE_LENGTH,
-	    );
-	    gameAddresses.push(TwoThirds001.address);
+    	0x2540099e9ed04aF369d557a40da2D8f9c2ab928D,
+      GAME_FEE_PERCENT,
+      BET_001,
+      MAX_PLAYERS, 
+      GAME_STAGE_LENGTH
+    );
+    gameAddresses.push(TwoThirds001.address);
 
 		const LUN1 = await gameFactory.createGame(
 			LUN_TYP,
-	    	0x2540099e9ed04aF369d557a40da2D8f9c2ab928D,
-	        GAME_FEE_PERCENT,
-	        BET_1,
-	        MAX_PLAYERS, 
-	        GAME_STAGE_LENGTH,
-	    );
+    	0x2540099e9ed04aF369d557a40da2D8f9c2ab928D,
+      GAME_FEE_PERCENT,
+      BET_1,
+      MAX_PLAYERS, 
+      GAME_STAGE_LENGTH
+    );
 		gameAddresses.push(LUN1.address);
 
 		const LUN01 = await gameFactory.createGame(
 			LUN_TYP,
-	    	0x2540099e9ed04aF369d557a40da2D8f9c2ab928D,
-	        GAME_FEE_PERCENT,
-	        BET_01,
-	        MAX_PLAYERS, 
-	        GAME_STAGE_LENGTH,
-	    );
+    	0x2540099e9ed04aF369d557a40da2D8f9c2ab928D,
+      GAME_FEE_PERCENT,
+      BET_01,
+      MAX_PLAYERS, 
+      GAME_STAGE_LENGTH
+    );
 		gameAddresses.push(LUN01.address);
 
 		const LUN001 = await gameFactory.createGame(
 			LUN_TYP,
-	    	0x2540099e9ed04aF369d557a40da2D8f9c2ab928D,
-	        GAME_FEE_PERCENT,
-	        BET_001,
-	        MAX_PLAYERS, 
-	        GAME_STAGE_LENGTH,
-	    );
-	    gameAddresses.push(LUN001.address);
+    	0x2540099e9ed04aF369d557a40da2D8f9c2ab928D,
+      GAME_FEE_PERCENT,
+      BET_001,
+      MAX_PLAYERS, 
+      GAME_STAGE_LENGTH
+    );
+    gameAddresses.push(LUN001.address);
 	})
 };
